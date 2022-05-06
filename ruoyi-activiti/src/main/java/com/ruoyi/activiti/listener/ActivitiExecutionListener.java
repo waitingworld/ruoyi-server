26dc005b872cce6908e67b197a718da752eac661
geshi <707535808@qq.com>
2022-05-06 16:44:28 +0800

    添加监听器

--- ruoyi-activiti/src/main/java/com/ruoyi/activiti/listener/ActivitiExecutionListener.java
+++ ruoyi-activiti/src/main/java/com/ruoyi/activiti/listener/ActivitiExecutionListener.java
@@ -2,6 +2,7 @@ package com.ruoyi.activiti.listener;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
+import org.activiti.engine.delegate.Expression;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,10 +10,16 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ActivitiExecutionListener implements ExecutionListener {
+    // 定义一个流程注入的表达式
+    private Expression expression;
     @Override
     public void notify(DelegateExecution delegateExecution) {
         System.out.println("xml任务：" + delegateExecution.getId() + " ActivitiListenner" + this.toString());
-
+        System.out.println("expression：" + expression.getExpressionText());//输出字符串表达式的内容
+//        eventName:
+//        start：开始时触发
+//        end：结束时触发
+//        take：主要用于监控流程线，当流程流转该线时触发
         String eventName = delegateExecution.getEventName();
         System.out.println("===="+eventName+"=====");
     }
--- ruoyi-activiti/src/main/java/com/ruoyi/activiti/listener/ActivitiTaskListener.java
+++ ruoyi-activiti/src/main/java/com/ruoyi/activiti/listener/ActivitiTaskListener.java
@@ -16,5 +16,25 @@ public class ActivitiTaskListener implements TaskListener {
         System.out.println("getEventName" + delegateTask.getEventName());
         System.out.println("getCategory" + delegateTask.getCategory());
         System.out.println("getDelegationState" + delegateTask.getDelegationState());
+        String event = delegateTask.getEventName();
+//        event:
+//        create：任务创建时触发，此时所有属性已被设置完毕
+//        assignment：任务被委派给某人后触发，如通过变量的方式设置处理人时会触发，先于create事件触发
+//        complete：在任务完成后，且被从运行时数据（runtime data）中删除前触发。
+//        delete：在任务将要被删除之前发生。
+        switch (event) {
+            case "create" :
+                System.out.println("create event");
+                break;
+            case "assignment" :
+                System.out.println("assignment event");
+                break;
+            case "complete" :
+                System.out.println("complete event");
+                break;
+            case "delete" :
+                System.out.println("delete event");
+                break;
+        }
     }
 }
